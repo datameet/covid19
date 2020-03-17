51f57449cfe0b867ff69bcb5e98ea3221d17908e
Thejesh GN <i@thejeshgn.com>
2020-03-17 22:04:19 +0530

    fixes

--- code/setup.sh
+++ code/setup.sh
@@ -1,3 +1,4 @@
 digdag secrets --project mohfw_data_scraper --set pushover_api_token
 digdag secrets --project mohfw_data_scraper --set pushover_user_key
 digdag secrets --project mohfw_data_scraper --set covid_db_full_url
+digdag secrets --project mohfw_data_scraper --set archive_folder_path
