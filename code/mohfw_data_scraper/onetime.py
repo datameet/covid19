be2de32df46db936cc473be177e1c369d70edd0a
Thejesh GN <i@thejeshgn.com>
2020-03-26 17:32:16 +0530

    icmr_testing_status.json

---
+++ code/mohfw_data_scraper/onetime.py
@@ -0,0 +1,38 @@
+import requests
+import re 
+import os.path
+from os import path
+import bs4
+from bs4 import BeautifulSoup
+import couchdb
+
+# I scripted this in urgency, So code quality or the logic isn't great.
+# This is mostly a hack. 
+
+# To RUN
+# Clone the project
+# Set up a virual environment 
+# Install everything in requirements
+# setup pushover_api_token, pushover_user_key and covid_db_full_url in env
+# needs couchdb
+# run this by calling `python3 scraper.py`
+# You can also schedule it using the digdag
+
+pushover_api_token =str(os.environ.get("pushover_api_token"))
+pushover_user_key = str(os.environ.get("pushover_user_key"))
+pushover_url = "https://api.pushover.net/1/messages.json"
+covid_db_full_url = str(os.environ.get("covid_db_full_url"))
+archive_folder_path = str(os.environ.get("archive_folder_path")) 
+force_run = False
+
+couchdb_db_name = "covid19"
+couch = couchdb.Server(covid_db_full_url)
+database = couch[couchdb_db_name]
+
+for d in database:
+	if str(d).startswith("2020"):
+		data = database[d]
+		data["source"] = "mohfw"
+		data["type"] = "cases"
+		database.save(data)
+		print(dict(data))
