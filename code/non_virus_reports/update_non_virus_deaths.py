b997bef500cd513321378b5382a940e267ac7693
Thejesh GN <i@thejeshgn.com>
2020-05-06 21:47:45 +0530

    Updated the non virus data

--- code/non_virus_reports/update_non_virus_deaths.py
+++ code/non_virus_reports/update_non_virus_deaths.py
@@ -89,7 +89,7 @@ def getDateTimeObject(passed_string):
 
 
 file_name = non_virus_archive_folder_path.format("non-virus-deaths.tsv")
-batch_to_process = "SET_MAY_3"
+batch_to_process = "SET_MAY_6"
 message = ""
 print("============================================{batch_to_process}===================================".format(batch_to_process=batch_to_process))
 with open(file_name) as csv_file:
