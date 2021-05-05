26b19f44509f24d48fa50d54517a5db038811a2d
Thejesh GN <i@thejeshgn.com>
2021-05-05 23:20:49 +0530

    updated data and added daily report scraper

---
+++ code/vaccinations/daily_report_parser.sh
@@ -0,0 +1,2 @@
+tabula-java -a 89.0,49.0,161.171,488.719 -p 1 "../../downloads/mohfw-backup/cumulative_vaccination_coverage/$1" > "$1_vaccine_country.csv"
+tabula-java -a 170.0,49.0,783.821,488.719 -p 1 "../../downloads/mohfw-backup/cumulative_vaccination_coverage/$1" > "$1_vaccine_states.csv"
