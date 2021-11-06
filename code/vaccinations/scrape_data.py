ece52a522b8ebd839d027ee7f043f778315c9704
Thejesh GN <i@thejeshgn.com>
2021-11-06 15:01:47 +0530

    updated data

--- code/vaccinations/scrape_data.py
+++ code/vaccinations/scrape_data.py
@@ -224,5 +224,5 @@ def parse_state_data(file_name):
 
 if __name__ == "__main__":
     #parse_all_country_again()
-    parse_country_data(file_name="2021-11-05-at-07-00-AM.pdf")
+    parse_country_data(file_name="2021-11-06-at-07-00-AM.pdf")
     #parse_state_data(file_name="2021-02-25-at-07-00-AM.pdf")
