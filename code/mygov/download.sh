169e72c37edf31db724785ce627a91953127a1d2
Thejesh GN <i@thejeshgn.com>
2021-11-17 18:55:54 +0530

    updated data

--- code/mygov/download.sh
+++ code/mygov/download.sh
@@ -1,1 +1,4 @@
 wget --output-document=../../downloads/mygov-in/`date +%Y-%m-%d`.html https://www.mygov.in/covid-19/
+wget --output-document=../../downloads/mygov-in/`date +%Y-%m-%d`-vaccine_counts_today.json https://www.mygov.in/sites/default/files/covid/vaccine/vaccine_counts_today.json
+wget --output-document=../../downloads/mygov-in/`date +%Y-%m-%d`-covid_state_counts_ver1.json https://www.mygov.in/sites/default/files/covid/covid_state_counts_ver1.json
+
