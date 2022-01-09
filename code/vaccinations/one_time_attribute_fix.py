b2c16243a8d85e42393da325dca0740667c2d49e
Thejesh GN <i@thejeshgn.com>
2022-01-09 12:29:23 +0530

    Added first_dose_15_18 to vaccination data

--- code/vaccinations/one_time_attribute_fix.py
+++ code/vaccinations/one_time_attribute_fix.py
@@ -7,20 +7,32 @@ couchdb_db_name = "covid19"
 couch = couchdb.Server(covid_db_full_url)
 database = couch[couchdb_db_name]
 
-
-# Opening JSON file
-with open('../../data/mohfw_vaccination_status.json') as json_file:
-    dataset = json.load(json_file)
-    rows = dataset["rows"]
-    for data_row in rows:
-        data = data_row["value"]
-        if "1stdose" in data:
-            data["first_dose"] = data["1stdose"]
-            del data["1stdose"]
-        if "2nddose" in data:
-            data["second_dose"] = data["2nddose"]
-            del data["2nddose"]
-        print(data)
-        #database.save(data)
+## FIX 1 - Rename attributes for better support
+# with open('../../data/mohfw_vaccination_status.json') as json_file:
+#     dataset = json.load(json_file)
+#     rows = dataset["rows"]
+#     for data_row in rows:
+#         data = data_row["value"]
+#         if "1stdose" in data:
+#             data["first_dose"] = data["1stdose"]
+#             del data["1stdose"]
+#         if "2nddose" in data:
+#             data["second_dose"] = data["2nddose"]
+#             del data["2nddose"]
+#         print(data)
+#         #database.save(data)
   
   
+## FIX 2 - 2022-01-09, first_dose_15_18 = null when it doesnt exist
+# with open('../../data/mohfw_vaccination_status.json') as json_file:
+#     dataset = json.load(json_file)
+#     rows = dataset["rows"]
+#     for data_row in rows:
+#         data = data_row["value"]
+#         if "first_dose_15_18" in data:
+#             pass
+#         else:
+#             data["first_dose_15_18"] = None
+            
+#         print(data)
+#         #database.save(data)
