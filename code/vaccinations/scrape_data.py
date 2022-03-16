1709e753b77b28a8890badbae6091755d92e2636
Thejesh GN <i@thejeshgn.com>
2022-03-16 07:23:26 +0530

    updated data

--- code/vaccinations/scrape_data.py
+++ code/vaccinations/scrape_data.py
@@ -17,7 +17,7 @@ couchdb_db_name = "covid19"
 couch = couchdb.Server(covid_db_full_url)
 database = couch[couchdb_db_name]
 
-FILE_NAME = "2022-03-14-at-07-00-AM.pdf"
+FILE_NAME = "2022-03-15-at-07-00-AM.pdf"
 
 states = {}
 states["Andhra Pradesh"]="AP"
@@ -106,7 +106,7 @@ def get_country_data(file_name):
     data_row = None
     print(len(rows))    
     if report_time > "2022-03-13":
-        data_row = rows[4]
+        data_row = rows[3]
     elif "2021-02-25" in report_time or "2021-02-28" in report_time :
         data_row = rows[0]
     elif "2021-02-26" in report_time or "2021-02-27" in report_time :
@@ -129,7 +129,9 @@ def get_country_data(file_name):
 
     start = 1
 
-    if "2021-02-25" in report_time:
+    if report_time > "2022-03-13":
+        start = 0
+    elif "2021-02-25" in report_time:
         start = 3
     elif "2021-02-26" in report_time or "2021-02-27" in report_time or "2021-02-28" in report_time:
         start = 2
