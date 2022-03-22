78ed35d5580f98e3269684db0699bade1349010d
Thejesh GN <i@thejeshgn.com>
2022-03-22 20:02:49 +0530

    updated data

--- code/vaccinations/scrape_data.py
+++ code/vaccinations/scrape_data.py
@@ -17,7 +17,7 @@ couchdb_db_name = "covid19"
 couch = couchdb.Server(covid_db_full_url)
 database = couch[couchdb_db_name]
 
-FILE_NAME = "2022-03-21-at-07-00-AM.pdf"
+FILE_NAME = "2022-03-22-at-07-00-AM.pdf"
 
 states = {}
 states["Andhra Pradesh"]="AP"
